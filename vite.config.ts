2cae7cbcecf11a51d813bbe3a08b53b98f68adcf
Nobsmoke123 <donaldakobundu@gmail.com>
2025-11-21 01:33:50 +0100

    Update Vite configuration to set API proxy target to localhost for local development

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     // setting up a proxy in vite
     proxy: {
       "/api": {
-        target: BACKEND_URL,
+        target: "http://localhost:5001",
         rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: true,
       },
