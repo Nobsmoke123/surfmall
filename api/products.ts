0ab0afea70c8a3a5f8d454749d3a4360fb0c2451
Nobsmoke123 <donaldakobundu@gmail.com>
2025-11-21 01:51:35 +0100

    debugging

--- api/products.ts
+++ api/products.ts
@@ -2,12 +2,13 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 import path from "path";
 import fs from "fs";
+import type { Product } from "../src/types/Product";
 
 const dbPath = path.join(process.cwd(), "src/data/db.json");
 
 export default function handler(_req: VercelRequest, res: VercelResponse) {
   const raw = fs.readFileSync(dbPath, "utf-8");
-  const db = JSON.parse(raw);
+  const db: { products: Product[] } = JSON.parse(raw);
 
-  res.status(200).json(db);
+  res.status(200).json(db.products);
 }
